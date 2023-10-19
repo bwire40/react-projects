91db2c107f2cec368c44ce73853b05891bf5a27e
bwire40 <emmanuelbwire80@gmail.com>
2023-10-19 10:43:22 +0300

    Installed tailwindcss and other dependencies

--- to-do-list-app/src/index.js
+++ to-do-list-app/src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const el = document.getElementById("root");
+
+const root = ReactDOM.createRoot(el);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
