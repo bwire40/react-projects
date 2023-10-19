870925ccd43efa86246b87c9e4597727e4f078f4
bwire40 <emmanuelbwire80@gmail.com>
2023-10-19 15:13:03 +0300

    meals api app commit

---
+++ meal-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import About from "./components/About";
+
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div>
+      <Main />
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ meal-app/src/components/About.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <section className="bg-white py-8">
+        <div className="container py-8 px-6 mx-auto">
+          <a
+            className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl mb-8"
+            href="#"
+          >
+            About
+          </a>
+
+          <p className="mt-8 mb-8">
+            Incididunt nisi ex quis minim dolore enim aliquip laboris fugiat
+            laboris. Reprehenderit consequat magna eu deserunt labore tempor
+            mollit. Laboris voluptate tempor voluptate laborum veniam consequat.
+            <br />
+          </p>
+
+          <p className="mb-8">
+            Lorem ipsum dolor sit amet, consectetur
+            <a href="#">random link</a> adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Vel risus commodo
+            viverra maecenas accumsan lacus vel facilisis volutpat. Vitae
+            aliquet nec ullamcorper sit. Nullam eget felis eget nunc lobortis
+            mattis aliquam. In est ante in nibh mauris. Egestas congue quisque
+            egestas diam in. Facilisi nullam vehicula ipsum a arcu. Nec nam
+            aliquam sem et tortor consequat. Eget mi proin sed libero enim seda
+            faucibus turpis in. Hac habitasse platea dictumst quisque. In
+            aliquam sem fringilla ut. Gravida rutrum quisque non tellus orci ac
+            auctor augue mauris. Accumsan lacus vel facilisis volutpat est velit
+            egestas dui id. At tempor commodo ullamcorper a. Volutpat commodo
+            sed egestas egestas fringilla. Vitae congue eu consequat ac.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default About;
